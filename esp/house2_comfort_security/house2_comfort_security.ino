a8aceb9be0ea9c25edba64ebb356929f29368425
Christoph Astlinger <c.astlinger@outlook.com>
2025-12-15 23:49:54 +0100

    remove hd module and merge with mod common

--- esp/house2_comfort_security/house2_comfort_security.ino
+++ esp/house2_comfort_security/house2_comfort_security.ino
@@ -16,16 +16,16 @@
 // Load Libraries
 #include <Arduino.h>
 #include "config.h"
-#include "hardware.h"
 #include "mod_wifi.h"
 #include "mod_lcd.h"
 #include "mod_rfid.h"
 #include "mod_window.h"
 #include "mod_door.h"
 #include "mod_mqtt.h"
+#include "mod_common.h"
 
+// Setup function
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   initLcd();
   initWiFi();
@@ -33,10 +33,13 @@ void setup() {
   initRFID();
   initWindow();
   initDoor();
+  InitCommon();
+  printTempLcd("Haus 2 bereit","",5000);
+  Serial.println("Haus 2 bereit");
 }
 
+// Main loop
 void loop() {
-  // put your main code here, to run repeatedly:
   wiFiLoop();
   loopMqtt();
   lcdUpdate();
